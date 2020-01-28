271107cbe05bfa28e87677e64357671cb19654f9
amitoj047 <60392387+amitoj047@users.noreply.github.com>
2020-01-28 21:05:42 +0530

    Add files via upload

---
+++ github.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# The purpose of this notebook is to be used as Capstone Project and thus will be mainly used for this project
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
